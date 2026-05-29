30814c92b9fedb023c4fa2c43fc8a3d804dad4d7
47Mags47 <gord_ka47@mail.ru>
2026-05-29 14:53:21 +0700

    Поправил сидеры

--- database/seeders/RootSeeder.php
+++ database/seeders/RootSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\UserRole;
 use App\Models\User;
-use App\Models\Config;
+use App\Models\Division;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
@@ -21,8 +21,19 @@ class RootSeeder extends Seeder
             'middle_name' => '',
             'email' => 'root',
             'password' => Hash::make('root'),
-            'division_id' => null,
+            'division_id' => Division::count() > 0 ? 1: null,
             'role_id' => UserRole::byCode('admin')->id,
+            'email_verified_at' => now(),
+        ]);
+
+        User::create([
+            'first_name' => 'admin',
+            'last_name' => '',
+            'middle_name' => '',
+            'email' => 'admin',
+            'password' => Hash::make('admin'),
+            'division_id' => Division::count() > 0 ? 1: null,
+            'role_id' => UserRole::byCode('division_admin')->id,
             'email_verified_at' => now(),
         ]);
     }
