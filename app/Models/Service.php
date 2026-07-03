2371f6238cecbccc2a12da480a226a44b52e5540
mubait <kuz2001min@mail.ru>
2026-07-03 09:22:17 +0700

    fix: исправил availableTime

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -34,22 +34,21 @@ class Service extends Model
     public function getAvailableTimeFromUser(User $worker, CarbonImmutable $date)
     {
         $shedule = $worker->shedules()->where('day_of_the_week_id', $date->dayOfWeek())->first();
-
-        if($shedule === null)
-            return [];
-
         $subscribes = $worker->subscribes()
             ->whereBetween("start_at", [$date->startOfDay(), $date->endOfDay()])
             ->orderBy('start_at')
             ->get();
+
+        // Получаем длительность текущей услуги
         $step = $this->duration->format('H') . ' hours '
             . $this->duration->format('i') . ' minutes '
             . $this->duration->format('s') . ' seconds';
 
+        // Получаем занятое время
         $busyTimes = $subscribes->map(
-            function ($subscribe) {
+            function ($subscribe) use ($step) {
                 return [
-                    'start' => $subscribe->start_at->format('H:i'),
+                    'start' => $subscribe->start_at->sub($step)->format('H:i'),
                     'end' => $subscribe->start_at
                         ->addHours((int) $subscribe->service->duration->format('H'))
                         ->addMinutes((int) $subscribe->service->duration->format('i'))
@@ -58,30 +57,39 @@ class Service extends Model
             }
         );
 
+        // Получаем время обеда
         if ($shedule->lunch_start and $shedule->lunch_end)
             $busyTimes[] = [
                 'start' => $shedule->lunch_start->sub($step)->format('H:i'),
                 'end' => $shedule->lunch_end->format('H:i'),
             ];
 
-        $availableTimes = [];
-        $currentStep = $shedule->date_start->format('H:i');
+        // Сортируем массив по времени начала
+        $busyTimes = $busyTimes->sort(fn($current, $next) => $current['start'] > $next['start']);
 
+        // Инвертируем массив заняго времени в массив свободного
+        $availableTimes = [];
+
+        $currentStep = $shedule->date_start->format('H:i');
         foreach ($busyTimes as $busyTime) {
 
             if ($currentStep < $busyTime['start'])
-                $availableTimes[] = ['start' => $currentStep, 'end' => $busyTime['start']];
+                $availableTimes[] = [
+                    'start' => $currentStep,
+                    'end' => $busyTime['start']
+                ];
 
-            if ($currentStep < $busyTime['end'])
+            if ($currentStep <= $busyTime['end'])
                 $currentStep = $busyTime['end'];
         }
+
         $availableTimes[] = [
             'start' => $busyTimes->last()['end'],
             'end' => $shedule->date_end->sub($step)->format('H:i')
         ];
 
-
-        $availableTimes = collect($availableTimes)->map(function ($availableTimes) use ($step) {
+        // Преобразуем масив доступного времени в список
+        $availableTimes = collect($availableTimes)->map(function ($availableTimes) use ($step, $shedule) {
             $period = new CarbonPeriod($availableTimes['start'], $step, $availableTimes['end']);
 
             return collect($period->toArray())->map(fn($date) => $date->format('H:i'));
